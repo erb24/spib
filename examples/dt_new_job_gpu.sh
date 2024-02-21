fa999e98e6966b10f49f6b3bc128f83b3abb8481
ebeyerle <ebeyerle@umd.edu>
2024-02-21 10:59:26 -0500

    add spib analysis files

---
+++ examples/dt_new_job_gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -t 12:00:00
+#SBATCH --job-name=hp35
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBTACH -n 16
+#SBATCH --gpus=a100:1
+#SBATCH -p gpu
+
+source /scratch/zt1/project/tiwary-prj/user/ebeyerle/anaconda3/etc/profile.d/conda.sh
+
+conda activate msmbuilder #/scratch/zt1/project/tiwary-prj/user/dwang97/.conda/envs/spib_msm
+
+export OMP_NUM_THREADS=1
+
+#python run_spib_msm_cv.py
+
+python dt_run_spib_msm_cv.py
